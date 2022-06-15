f33fa49a615de3cd34d0da6ade0d22f68fd4e0d5
Filip Landgren <filip.landgren@gmail.com>
2022-06-15 21:19:31 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ formElement.addEventListener('submit', (event) => {
   messageOne.style.display = 'block';
   messageTwo.style.display = 'none';
 
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const geocode = require('./utils/geocode');
 // __filename is filepath to this file
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -89,6 +90,6 @@ app.get('*', (req, res) => {
 });
 
 // start server
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(port, () => {
+  console.log('Server running on port ' + port);
 });
